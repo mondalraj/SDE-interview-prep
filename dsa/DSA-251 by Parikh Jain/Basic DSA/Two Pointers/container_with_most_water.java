799bde0e2f54bfc4d6ea09382eb92b59beafe40e
mondalraj <mondal021002rajib@gmail.com>
2023-06-11 14:02:05 +0530

    container with most water

---
+++ dsa/DSA-251 by Parikh Jain/Basic DSA/Two Pointers/container_with_most_water.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/container-with-most-water/description/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int max_area = 0;
+        int start = 0;
+        int end = height.length - 1;
+        for(int i = start, j = end; i < j; ){
+            int area = (j - i) * Math.min(height[i], height[j]);
+            if(area > max_area){
+                max_area = area;
+            }
+            if(height[i] > height[j]){
+                j--;
+            }else {
+                i++;
+            }
+        }
+        return max_area;
+    }
+}
